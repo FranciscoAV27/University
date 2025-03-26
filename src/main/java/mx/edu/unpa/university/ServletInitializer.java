0d210d139a69b68611b4322ffe80e6cb9597d7e4
Valentino1746 <valentinomatamoros2@gmail.com>
2025-03-26 17:21:42 -0600

    cambiooooo

--- src/main/java/mx/edu/unpa/university/ServletInitializer.java
+++ src/main/java/mx/edu/unpa/university/ServletInitializer.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package mx.edu.unpa.university;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
--- src/main/java/mx/edu/unpa/university/UniversityApplication.java
+++ src/main/java/mx/edu/unpa/university/UniversityApplication.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package mx.edu.unpa.university;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
